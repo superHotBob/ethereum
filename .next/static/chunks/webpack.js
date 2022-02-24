b291ddc5adc52a329930c93fd2209c1d2b154fbc
AlexHor1909 <19ham09@mail.com>
2022-02-24 11:51:25 +0300

    new_gas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88ddb604fa45310f"; }
+/******/ 		__webpack_require__.h = function() { return "aa22666fed57c97a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
