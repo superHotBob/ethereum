35bd61d72bd181c7f80f7809718315bb5bb3becb
AlexHor1909 <19ham09@mail.com>
2022-02-24 11:36:28 +0300

    my_last_commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdfcdcc47ea2deeb"; }
+/******/ 		__webpack_require__.h = function() { return "88ddb604fa45310f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
